c59621a2a54b94e5e6094ec1f774dce10b12ad48
quarkmarino <quarkmarino@vm-0.quarkmarino.koding.kd.io>
2014-09-17 17:15:36 +0000

    adds the fotter widgets

---
+++ app/views/widgets/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<section id="footer">
+  
+  <!-- Go to Top -->
+  <div id="go-to-top" onclick="scroll_to_top();"><span class="icon glyphicon glyphicon-chevron-up"></span></div>
+  
+  <ul class="social-icons">
+    <li><a href="#" target="_blank" title="Facebook"><img src="assets/images/theme_images/social_icons/facebook.png" alt="Facebook"></a></li>
+    <li><a href="#" target="_blank" title="Twitter"><img src="assets/images/theme_images/social_icons/twitter.png" alt="Twitter"></a></li>
+    <li><a href="#" target="_blank" title="Google+"><img src="assets/images/theme_images/social_icons/googleplus.png" alt="Google+"></a></li>
+  </ul>
+  
+  <!-- copyright text -->
+  <div class="footer-text-line">&copy; 2014 Twilli | Air</div>
+</section>
+<!-- end: Footer -->   
