def51db049ba0d9a974d8c8779e17f1fcea19f68
Mariano Escalera (quarkmarino) <quarkmarino@gmail.com>
2014-09-25 20:04:20 +0000

    fixes gallery main image and gallery main image form alternation

--- app/routes.php
+++ app/routes.php
@@ -13,13 +13,13 @@
 
 Route::get('/', 'Controllers\HomeController@index');
 
+Route::resource('galleries', 'Controllers\Admin\GalleriesController', array('only' => array('index', 'edit')));
+Route::get('galleries/{galleries}/delete', array('uses' =>'Controllers\Admin\GalleriesController@destroy', 'as' =>'galleries.destroy'));
 
-
-//Route::group(array('group' => 'admin'), function(){
-  Route::resource('galleries', 'Controllers\Admin\GalleriesController', array('except' => array('destroy', 'create', 'show' )));
-  Route::get('galleries/{galleries}/delete',array('uses' =>'Controllers\Admin\GalleriesController@destroy', 'as' =>'galleries.destroy' ));
-
-  Route::resource('galleries.images', 'Controllers\Admin\Gallery\ImagesController', array('except' => array('create', 'show', 'destroy')));
-  Route::get('galleries/{galleries}/images/{images}/delete',array('uses' =>'Controllers\Admin\Gallery\ImageController@destroy', 'as' =>'galleries.images.destroy' ));  
-
-//});
+Route::resource('galleries.images', 'Controllers\Admin\Gallery\ImagesController', array('only' => array('index', 'edit')));
+Route::get('galleries/{galleries}/images/{images}/delete', array('uses' =>'Controllers\Admin\Gallery\ImagesController@destroy', 'as' =>'galleries.images.destroy'));
+  
+Route::group(array('before' => 'csrf'), function() {
+	Route::resource('galleries', 'Controllers\Admin\GalleriesController', array('only' => array('store', 'update')));
+	Route::resource('galleries.images', 'Controllers\Admin\Gallery\ImagesController', array('only' => array('store', 'update')));
+});
